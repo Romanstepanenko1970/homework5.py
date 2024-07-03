306ee7eb671c243fe98d89bd9f73aa2df87abcc9
Роман <roman.stepanenko@mail.ru>
2024-07-03 08:19:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+immutable_var = 1, 2, 5, 'and', 'apple', True
+print(immutable_var)
+mutable_list = [3, 4, 7, 'banana', 'watermelon']
+print(mutable_list)
+mutable_list [3] = 'apple'
+print(mutable_list)
